01c4e0d5716e36b66eb6a021491853d1034c8def
Pablo Widlert <pablo@probarium.se>
2024-02-06 07:37:12 +0100

    Update CustomIndexHtml.sh

--- CustomIndexHtml.sh
+++ CustomIndexHtml.sh
@@ -9,7 +9,7 @@ cat > index.html << EOF
 
 <!DOCTYPE html>
 <meta charset="utf-8">
-<meta http-equiv="refresh" content="0; URL=https://pablowidlert.github.io/playwright-example-with-typescript/${1}/">
+<meta http-equiv="refresh" content="0; URL=https://pablowidlert.github.io/volvo-docker-playwright/${1}/">
 <meta http-equiv="Pragma" content="no-cache"><meta http-equiv="Expires" content="0">
 
 EOF
